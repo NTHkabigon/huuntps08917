cb7f0349d08d9990aa8bad73c5ab18186f5ed507
NguyenTinHuu <51940259+NguyenTinHuu@users.noreply.github.com>
2019-08-17 13:46:19 +0700

    Add files via upload

---
+++ js/checkgopy.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $("#gopy").click(function() {
+       var username = $("#email").val();
+       var password = $("#hoten").val();
+       var noidung =$("#noidung").val();
+       if(username =='' || password == '' || noidung ==''){
+           alert('vui lòng nhập đầy đủ dữ liệu')
+           return false;
+       }
+       else{
+           alert('Góp ý của bạn đã được gửi. Chân Thành Cảm ơn!')
+           return
+       }
+
+    });
+});
